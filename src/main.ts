72e49899ac8b7a35b9f501f8e47feeeed509903e
100Roads <93051531+100Roads@users.noreply.github.com>
2024-07-11 21:41:31 +0200

    privacy policy fix

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,31 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+  console.log('Scripting API ready');
+  console.log('Player tags: ',WA.player.tags)
+
+  const mapUrl = WA.room.mapURL
+  const root = mapUrl.substring(0, mapUrl.lastIndexOf("/"))
+
+  
 
     // Julia custom
-
-    WA.room.onEnterLayer("floor").subscribe(() => {
-        WA.room.hideLayer("roof");
-        WA.room.hideLayer("walls-bg-front");
-        WA.room.hideLayer("sign");
-      });
+   
+  WA.room.onEnterLayer("floor").subscribe(() => {
+    console.log("Entered to floor")
+    WA.room.hideLayer("roof");
+    WA.room.hideLayer("walls-bg-front");
+    WA.room.hideLayer("sign");
+  });
       
-    WA.room.onLeaveLayer("floor").subscribe(() => {
-        WA.room.showLayer("roof");
-        WA.room.showLayer("walls-bg-front");
-        WA.room.showLayer("facade-furniture-bg");
-        WA.room.showLayer("sign");
-      });
+  WA.room.onLeaveLayer("floor").subscribe(() => {
+      WA.room.showLayer("roof");
+      WA.room.showLayer("walls-bg-front");
+      WA.room.showLayer("facade-furniture-bg");
+      WA.room.showLayer("sign");
+    });
       WA.room.onEnterLayer("floor1").subscribe(() => {
         WA.room.hideLayer("roof1");
         WA.room.hideLayer("walls-bg-front1");
@@ -62,68 +66,11 @@ WA.onInit().then(() => {
         WA.room.showLayer("facade-furniture-bg");
       });
     
-    //Popup Liberary
-    WA.room.onEnterLayer('message-1').subscribe(() => {
-        currentPopup = WA.ui.openPopup("Addon1Pop","Library",[]);
-    })
-    WA.room.onLeaveLayer('message-1').subscribe(closePopup)
 
-    //Popup Ocean
-    WA.room.onEnterLayer('message-3').subscribe(() => {
-        currentPopup = WA.ui.openPopup("Addon3Pop","Ocean",[]);
-    })
-    WA.room.onLeaveLayer('message-3').subscribe(closePopup)
-
-    //Popup Playground
-    WA.room.onEnterLayer('message-2').subscribe(() => {
-        currentPopup = WA.ui.openPopup("Addon2Pop","Playground",[]);
-    })
-    WA.room.onLeaveLayer('message-2').subscribe(closePopup)
-
-    //Popup Amphitheater
-    WA.room.onEnterLayer('message-4').subscribe(() => {
-        currentPopup = WA.ui.openPopup("Addon4Pop","Amphitheater",[]);
-    })
-    WA.room.onLeaveLayer('message-4').subscribe(closePopup)
-
-
-    //Popup Science Cluster
-    WA.room.onEnterLayer('message-5').subscribe(() => {
-        currentPopup = WA.ui.openPopup("Addon5Pop","Science Cluster",[]);
-    })
-    WA.room.onLeaveLayer('message-5').subscribe(closePopup)
-
-    //Popup Club House
-    WA.room.onEnterLayer('message-6').subscribe(() => {
-        currentPopup = WA.ui.openPopup("Addon6Pop","Club House",[]);
-    })
-    WA.room.onLeaveLayer('message-6').subscribe(closePopup)
-      
-    WA.room.onEnterLayer('clockZone').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-    })
-
-    WA.room.onLeaveLayer('clockZone').subscribe(closePopup)
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-
-}).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 WA.ui.actionBar.addButton({
   id: 'privacy-btn',
   type: 'action',
-  imageSrc: 'public/images/favicon.svg',
+  imageSrc: root + '/lock.svg',
   toolTip: "Privacy Policy",
   callback: () => {
       WA.ui.modal.openModal({
@@ -135,4 +82,14 @@ WA.ui.actionBar.addButton({
       }, () => WA.ui.modal.closeModal())
   }
 });
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+      console.log('Scripting API Extra ready');
+      // Update current map name
+    
+  }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
 export {};
